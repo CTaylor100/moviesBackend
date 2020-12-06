176102be81869fce4c8fb8d74e5828725681e9b4
Christine Taylor <CTaylor100@gmail.com>
2020-12-05 19:10:59 -0500

    Movies Table Migration Done

---
+++ db/migrate/20201205235908_create_movies.rb
@@ -0,0 +1,9 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.integer :year
+      t.string :poster_url
+    end
+  end
+end
